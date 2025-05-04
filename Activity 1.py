9ccb47360ebd5a20998e222bc1d4f4ecc3d3e467
shiwu li <leeshiwu66@gmail.com>
2025-05-04 12:15:09 +1200

    update

--- Activity 1.py
+++
@@ -1,9 +0,0 @@
-import numpy as nu
-#===Activity One===
-arr=nu.arange(1,11)
-print("Original array: ", arr)
-print("Shape of array: ", arr.shape)
-print("Data type: ", arr.dtype)
-
-arr_doubled = arr*2
-print("Array elements multiplied by 2 ",arr_doubled)
